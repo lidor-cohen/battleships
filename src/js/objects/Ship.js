84acb5cd84b467648b617d9b2b1bf38905bebf15
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-07-23 21:19:46 +0300

    added a ship class

---
+++ src/js/objects/Ship.js
@@ -0,0 +1,19 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hits++;
+
+    if (this.isSunk()) {
+      this.sink();
+    }
+  }
+
+  isSunk = () => this.hits === this.length;
+
+  sink = () => (this.sunk = true);
+}
