159c1a34a988cf4dcdac2716abc5e1950f7d5265
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-07-26 18:32:00 +0300

    add: ability to revert back to last ship part selected

--- src/js/objects/Cell.js
+++ src/js/objects/Cell.js
@@ -1,38 +1,65 @@
 class Cell {
   constructor(type, i, j) {
-    const cell = document.createElement("button");
-    cell.classList.add("ship-part");
-    cell.classList.add("clickable-ship-part");
-    cell.classList.add(`${type.toLowerCase()}-ship-part`);
-    cell.id = `${type}-${i}-${j}`;
-    cell.style.cursor = "pointer";
-
-    this.element = cell;
-    this.#bindEvents();
-    this.i = i;
-    this.j = j;
-
     this.parts =
       type === "player"
         ? [
+            "",
             "vertical-top-ship",
             "vertical-body-ship",
             "vertical-bottom-ship",
             "horizontal-left-ship",
             "horizontal-body-ship",
             "horizontal-right-ship",
-            "",
           ]
         : ["", "hit", "miss"];
+
+    this.currPart = this.parts[0];
+    this.type = type;
+    this.i = i;
+    this.j = j;
+
+    this.element = this.#constructCell();
+    this.#bindEvents();
+  }
+
+  #constructCell() {
+    const cell = document.createElement("button");
+    cell.classList.add("ship-part");
+    cell.classList.add("clickable-ship-part");
+    cell.classList.add(`${this.type.toLowerCase()}-ship-part`);
+    cell.id = `${this.type}-${this.i}-${this.j}`;
+    cell.style.cursor = "pointer";
+
+    return cell;
   }
 
   #bindEvents() {
     this.element.addEventListener("click", () => {
-      let currPart = this.parts.shift();
-      this.parts.push(currPart);
+      this.currPart = this.parts.shift();
+      this.parts.push(this.currPart);
+      this.currPart = this.parts[0];
+
       this.element.innerHTML = `<div class="${
-        currPart.split("-")[0]
-      }-ship-part-art ${currPart}"></div>`;
+        this.currPart.split("-")[0]
+      }-ship-part-art ${this.currPart}"></div>`;
+
+      console.log(this.currPart);
+
+      // this.gameboard.board[i][j] = currPart;
+      // console.log(currPart, parts);
+    });
+    this.element.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+
+      this.currPart = this.parts.pop();
+      this.parts.unshift(this.currPart);
+      this.currPart = this.parts[0];
+
+      this.element.innerHTML = `<div class="${
+        this.currPart.split("-")[0]
+      }-ship-part-art ${this.currPart}"></div>`;
+
+      console.log(this.currPart);
 
       // this.gameboard.board[i][j] = currPart;
       // console.log(currPart, parts);
